ircd-hybrid/ircd-hybrid

commit c7e2b83020415e3c344193029bb86a5d151cef7e

- Restore fd_table. No longer allocate fde_t items from within any other structures like the AuthRequest, or Connection structure - struct AuthRequest once again is no longer allocated from within the Connection structure
git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@8338 82007160-df01-0410-b94d-b575c5fd34c7

src/listener.c
@@ -98,6 +98,9 @@ listener_accept_connection(fde_t *pfd, void *data)
   struct Listener *const listener = data;
 
   assert(listener);
+  assert(listener->fd == pfd);
+  assert(listener->fd);
+  assert(listener->fd->flags.open);
 
   /* There may be many reasons for error return, but
    * in otherwise correctly working environment the
@@ -109,7 +112,7 @@ listener_accept_connection(fde_t *pfd, void *data)
    * point, just assume that connections cannot
    * be accepted until some old is closed first.
    */
-  while ((fd = comm_accept(&listener->fd, &addr)) != -1)
+  while ((fd = comm_accept(listener->fd->fd, &addr)) != -1)
   {
     /*
      * check for connection limit
@@ -157,7 +160,7 @@ listener_accept_connection(fde_t *pfd, void *data)
   }
 
   /* Re-register a new IO request for the next accept .. */
-  comm_setselect(&listener->fd, COMM_SELECT_READ, listener_accept_connection,
+  comm_setselect(listener->fd, COMM_SELECT_READ, listener_accept_connection,
                  listener, 0);
 }
 
@@ -190,19 +193,19 @@ inetport(struct Listener *listener)
   /*
    * At first, open a new socket
    */
-  if (comm_open(&listener->fd, listener->addr.ss.ss_family, SOCK_STREAM, 0,
-                "Listener socket") == -1)
+  int fd = comm_socket(listener->addr.ss.ss_family, SOCK_STREAM, 0);
+  if (fd == -1)
   {
     report_error(L_ALL, "opening listener socket %s:%s",
                  listener_get_name(listener), errno);
     return 0;
   }
 
-  if (setsockopt(listener->fd.fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
+  if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
   {
     report_error(L_ALL, "setting SO_REUSEADDR for listener %s:%s",
                  listener_get_name(listener), errno);
-    fd_close(&listener->fd);
+    close(fd);
     return 0;
   }
 
@@ -212,25 +215,27 @@ inetport(struct Listener *listener)
    */
   lsin.ss_port = htons(listener->port);
 
-  if (bind(listener->fd.fd, (struct sockaddr *)&lsin, lsin.ss_len))
+  if (bind(fd, (struct sockaddr *)&lsin, lsin.ss_len))
   {
     report_error(L_ALL, "binding listener socket %s:%s",
                  listener_get_name(listener), errno);
-    fd_close(&listener->fd);
+    close(fd);
     return 0;
   }
 
-  if (listen(listener->fd.fd, HYBRID_SOMAXCONN))
+  if (listen(fd, HYBRID_SOMAXCONN))
   {
     report_error(L_ALL, "listen failed for %s:%s",
                  listener_get_name(listener), errno);
-    fd_close(&listener->fd);
+    close(fd);
     return 0;
   }
 
+  listener->fd = fd_open(fd, 1, "Listener socket");
+
   /* Listen completion events are READ events .. */
 
-  listener_accept_connection(&listener->fd, listener);
+  listener_accept_connection(listener->fd, listener);
   return 1;
 }
 
@@ -249,8 +254,11 @@ listener_find(int port, struct irc_ssaddr *addr)
         (!memcmp(addr, &listener->addr, sizeof(*addr))))
     {
       /* Try to return an open listener, otherwise reuse a closed one */
-      if (!listener->fd.flags.open)
+      if (listener->fd)
+      {
+        assert(listener->fd->flags.open);
         last_closed = listener;
+      }
       else
         return (listener);
     }
@@ -265,8 +273,13 @@ listener_find(int port, struct irc_ssaddr *addr)
 static void
 listener_close(struct Listener *listener)
 {
-  if (listener->fd.flags.open)
-    fd_close(&listener->fd);
+  if (listener->fd)
+  {
+    assert(listener->fd->flags.open);
+
+    fd_close(listener->fd);
+    listener->fd = NULL;
+  }
 
   listener->active = 0;
 
@@ -378,8 +391,11 @@ listener_add(int port, const char *vhost_ip, unsigned int flags)
   {
     listener->flags = flags;
 
-    if (listener->fd.flags.open)
+    if (listener->fd)
+    {
+      assert(listener->fd->flags.open);
       return;
+    }
   }
   else
   {

include/auth.h
@@ -32,14 +32,9 @@ enum { RFC1413_PORT   = 113 };  /**< As defined per rfc1413, IDENT server listen
 
 enum
 {
-  AM_IN_AUTH     = 0x00000001U,
-  AM_DOING_AUTH  = 0x00000002U,
-  AM_DNS_PENDING = 0x00000004U
+  AM_DOING_AUTH  = 0x00000001U,
+  AM_DNS_PENDING = 0x00000002U
 };
-
-#define SetInAuth(x)         ((x)->flags |= AM_IN_AUTH)
-#define ClearInAuth(x)       ((x)->flags &= ~AM_IN_AUTH)
-#define IsInAuth(x)          ((x)->flags &  AM_IN_AUTH)
 
 #define SetDNSPending(x)     ((x)->flags |= AM_DNS_PENDING)
 #define ClearDNSPending(x)   ((x)->flags &= ~AM_DNS_PENDING)
@@ -57,7 +52,7 @@ struct AuthRequest
   dlink_node node;        /**< Doubly linked list node. */
   unsigned int flags;     /**< Current state of request. */
   struct Client *client;  /**< Pointer to Client structure for request. */
-  fde_t fd;               /**< File descriptor for identd queries. */
+  fde_t *fd;              /**< File descriptor for identd queries. */
   uintmax_t timeout;      /**< Time when query expires. */
 };
 

include/client.h
@@ -340,7 +340,7 @@ struct Connection
     uintmax_t last_attempt;  /**< Last time the AWAY/INVITE/KNOCK/NICK request was issued */
   } away, invite, knock, nick;
 
-  struct AuthRequest auth;
+  struct AuthRequest *auth;
   struct Listener *listener;   /**< Listener accepted from */
   dlink_list        acceptlist; /**< Clients I'll allow to talk to me */
   dlink_list        watches;   /**< Chain of Watch pointer blocks */
@@ -349,7 +349,7 @@ struct Connection
   struct irc_ssaddr ip;
   int               aftype;    /**< Makes life easier for DNS res in IPV6 */
 
-  fde_t             fd;
+  fde_t            *fd;
 
   /* Anti-flood stuff. We track how many messages were parsed and how
    * many we were allowed in the current second, and apply a simple

include/fdlist.h
@@ -31,8 +31,7 @@
 #include "tls.h"
 
 
-enum { FD_DESC_SIZE = 128 };  /* hostlen + comment */
-enum { FD_HASH_SIZE = 1024 };
+enum { FD_DESC_SIZE = 128 };  /* HOSTLEN + comment */
 
 enum
 {
@@ -86,20 +85,18 @@ typedef struct _fde
   } connect;
 
   tls_data_t ssl;
-  struct _fde *hnext;
 } fde_t;
 
 extern int number_fd;
 extern int hard_fdlimit;
-extern fde_t *fd_hash[];
-extern fde_t *fd_next_in_loop;
+extern int highest_fd;
+extern fde_t *fd_table;
 
 extern void fdlist_init(void);
-extern fde_t *lookup_fd(int);
-extern void fd_open(fde_t *, int, int, const char *);
+extern fde_t *fd_open(int, int, const char *);
 extern void fd_close(fde_t *);
 extern void fd_dump(struct Client *, int, char *[]);
 extern void fd_note(fde_t *, const char *, ...);
 extern void close_standard_fds(void);
-extern void close_fds(fde_t *);
+extern void close_fds(void);
 #endif /* INCLUDED_fdlist_h */

include/listener.h
@@ -40,7 +40,7 @@ enum
 struct Listener
 {
   dlink_node node;           /**< Doubly linked list node */
-  fde_t fd;                  /**< Describe socket to event system */
+  fde_t *fd;                 /**< File descriptor */
   int port;                  /**< Listener IP port */
   int ref_count;             /**< Number of connection references */
   int active;                /**< Current state of listener */

include/s_bsd.h
@@ -53,10 +53,10 @@ extern void comm_settimeout(fde_t *, uintmax_t, void (*)(fde_t *, void *), void 
 extern void comm_setflush(fde_t *, uintmax_t, void (*)(fde_t *, void *), void *);
 extern void comm_checktimeouts(void *);
 extern void comm_connect_tcp(fde_t *, const char *, unsigned short, struct sockaddr *, int,
-                             void (struct _fde *, int, void *), void *, int, uintmax_t);
+                             void (fde_t *, int, void *), void *, int, uintmax_t);
 extern const char *comm_errstr(int);
-extern int comm_open(fde_t *, int, int, int, const char *);
-extern int comm_accept(fde_t *, struct irc_ssaddr *);
+extern int comm_socket(int, int, int);
+extern int comm_accept(int, struct irc_ssaddr *);
 
 /* These must be defined in the network IO loop code of your choice */
 extern void netio_init(void);

modules/core/m_nick.c
@@ -183,7 +183,7 @@ set_initial_nick(struct Client *source_p, const char *nick)
   hash_add_client(source_p);
 
   /* fd_desc is long enough */
-  fd_note(&source_p->connection->fd, "Nick: %s", source_p->name);
+  fd_note(source_p->connection->fd, "Nick: %s", source_p->name);
 
   if (!source_p->connection->registration)
     register_local_user(source_p);
@@ -259,7 +259,7 @@ change_local_nick(struct Client *source_p, const char *nick)
     watch_check_hash(source_p, RPL_LOGON);
 
   /* fd_desc is long enough */
-  fd_note(&source_p->connection->fd, "Nick: %s", source_p->name);
+  fd_note(source_p->connection->fd, "Nick: %s", source_p->name);
 }
 
 /*!

modules/core/m_server.c
@@ -44,6 +44,7 @@
 #include "parse.h"
 #include "memory.h"
 #include "modules.h"
+#include "fdlist.h"
 
 
 /*! Parses server flags to be potentially set
@@ -295,21 +296,21 @@ server_estab(struct Client *client_p)
     AddFlag(client_p, FLAGS_SERVICE);
 
   /* Show the real host/IP to admins */
-  if (tls_isusing(&client_p->connection->fd.ssl))
+  if (tls_isusing(&client_p->connection->fd->ssl))
   {
     /* Show the real host/IP to admins */
     sendto_realops_flags(UMODE_SERVNOTICE, L_ADMIN, SEND_NOTICE,
                          "Link with %s established: [TLS: %s] (Capabilities: %s)",
-                         client_get_name(client_p, SHOW_IP), tls_get_cipher(&client_p->connection->fd.ssl),
+                         client_get_name(client_p, SHOW_IP), tls_get_cipher(&client_p->connection->fd->ssl),
                          capab_get(client_p));
 
     /* Now show the masked hostname/IP to opers */
     sendto_realops_flags(UMODE_SERVNOTICE, L_OPER, SEND_NOTICE,
                          "Link with %s established: [TLS: %s] (Capabilities: %s)",
-                         client_get_name(client_p, MASK_IP), tls_get_cipher(&client_p->connection->fd.ssl),
+                         client_get_name(client_p, MASK_IP), tls_get_cipher(&client_p->connection->fd->ssl),
                          capab_get(client_p));
     ilog(LOG_TYPE_IRCD, "Link with %s established: [TLS: %s] (Capabilities: %s)",
-         client_get_name(client_p, SHOW_IP), tls_get_cipher(&client_p->connection->fd.ssl),
+         client_get_name(client_p, SHOW_IP), tls_get_cipher(&client_p->connection->fd->ssl),
          capab_get(client_p));
   }
   else
@@ -326,7 +327,7 @@ server_estab(struct Client *client_p)
          client_get_name(client_p, SHOW_IP), capab_get(client_p));
   }
 
-  fd_note(&client_p->connection->fd, "Server: %s", client_p->name);
+  fd_note(client_p->connection->fd, "Server: %s", client_p->name);
 
   sendto_server(client_p, 0, 0, ":%s SID %s 2 %s :%s%s",
                 me.id, client_p->name, client_p->id,

paigeadelethompson/UltraIRCd

commit 133f7697971b84e7f27368eb0dd5087be81315b8

- listener: fixed old bug in `find_listener` introduced in c7e2b83020415e3c344193029bb86a5d151cef7e. We're supposed to prioritize an active listener over closed ones.

src/listener.c
@@ -369,9 +369,9 @@ listener_find(const struct io_addr *addr)
     {
       /* Try to return an open listener, otherwise reuse a closed one */
       if (listener_is_active(listener))
-        last_closed = listener;
-      else
         return listener;
+
+      last_closed = listener;
     }
   }
 